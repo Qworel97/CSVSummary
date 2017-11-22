36909a10f17cbecc4ed3520719f8fb94736f6ad6
Dmytro_Shevchenko4 <Dmytro_Shevchenko4@epam.com>
2017-11-22 15:29:08 +0200

    Added some documentation

--- src/main/java/com/shevchenko/csvsummary/component/Cache.java
+++ src/main/java/com/shevchenko/csvsummary/component/Cache.java
@@ -2,13 +2,31 @@ package com.shevchenko.csvsummary.component;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Date;
 
 /**
+ * Simple cache interface
+ *
  * @author Dmytro_Shevchenko4
  */
 public interface Cache {
+    /**
+     * Puts file to cache. File should be remove after eviction
+     * @param file that should be cached
+     */
     void put(File file);
+
+    /**
+     * Retrieves file from cache. Extends its life time
+     * @param name file name
+     * @return file that was found in cache
+     * @throws FileNotFoundException if file is not in cache
+     */
     File get(String name) throws FileNotFoundException;
+
+    /**
+     * Extends life time for file with specified name
+     * @param name file name
+     * @return true if file exists
+     */
     boolean extend(String name);
 }
--- src/main/java/com/shevchenko/csvsummary/component/Parser.java
+++ src/main/java/com/shevchenko/csvsummary/component/Parser.java
@@ -4,13 +4,37 @@ import java.io.File;
 import java.io.IOException;
 
 /**
+ * Interface for CSV parser
+ *
  * @author Dmytro_Shevchenko4
  */
 public interface Parser {
 
+    /**
+     * Parses headers of file
+     *
+     * @param file file to parse
+     * @return string that contains all headers
+     * @throws IOException if file is not found
+     */
     String parseHeaders(File file) throws IOException;
 
-    Double summarize(File file, String header) throws IOException;
+    /**
+     * Return sum of row with specific header
+     * @param file   file to sum
+     * @param header header name
+     * @return sum of a row
+     * @throws IOException if file not found
+     * @throws NumberFormatException if some value is not double
+     * @throws IllegalArgumentException if header is not present
+     */
+    Double summarize(File file, String header) throws IOException, NumberFormatException, IllegalArgumentException;
 
+    /**
+     * Checks if file contains valid double data
+     * @param file to check
+     * @return true if all (except headers) are doubles
+     * @throws IOException if file is not found
+     */
     boolean isValid(File file) throws IOException;
 }
