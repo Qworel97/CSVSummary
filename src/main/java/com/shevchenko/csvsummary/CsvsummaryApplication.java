6216318c105b2b49384378f76c633dd5c5b43eb9
Dmytro_Shevchenko4 <Dmytro_Shevchenko4@epam.com>
2017-11-22 10:39:05 +0200

    Initial commit

---
+++ src/main/java/com/shevchenko/csvsummary/CsvsummaryApplication.java
@@ -0,0 +1,12 @@
+package com.shevchenko.csvsummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvsummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvsummaryApplication.class, args);
+	}
+}
