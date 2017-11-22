d8f2a0b5ab28b12f8c785dc51f0a8f318a161bfa
Dmytro_Shevchenko4 <Dmytro_Shevchenko4@epam.com>
2017-11-22 15:37:03 +0200

    Small fixes

--- src/main/java/com/shevchenko/csvsummary/entity/Messages.java
+++ src/main/java/com/shevchenko/csvsummary/entity/Messages.java
@@ -10,7 +10,7 @@ public enum Messages {
     FILE_WAS_NOT_FOUND("You file was not found. Maybe it expired"),
     NO_FILE_SELECTED_ERROR("No file selected"),
     FILE_IS_TOO_BIG("File is to big. Max is 1MB"),
-    WRONG_FILE_DATA("File contains double. You can work with previous uploaded file if it existed"),
+    WRONG_FILE_DATA("File is not valid. You can work with previous uploaded file if it existed"),
     FILE_CONTAINS_NOT_DOUBLE_ERROR("File contains wrong data that can not be converted to double"),
     NOT_EXISTING_HEADER("This header does not exist. Try one from list below"),
     SUCCESSFULLY_UPLOADED_FILE("You have successfully uploaded file");
